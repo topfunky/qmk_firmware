a68bef86824a0104353bd31257654b21c2cdf90b
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2025-07-15 20:21:39 -0700

    wip: implement layers 1 and 3

--- keyboards/keebio/iris_lm/keymaps/topfunky/config.h
+++ keyboards/keebio/iris_lm/keymaps/topfunky/config.h
@@ -6,7 +6,13 @@
 #define RGB_MATRIX_LAYERS_RETAIN_VAL
 
 #define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT // Sets the default mode, if none has been set
-#define RGB_MATRIX_DEFAULT_SPD 10 // Sets the default animation speed, if none has been set
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define RGB_MATRIX_DEFAULT_SPD 10 // Default animation speed out of 255
 
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT
+
+#undef RGB_MATRIX_MAXIMUM_BRIGHTNESS
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 96 // 255 is full brightness
+
+// Limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+#define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5
--- keyboards/keebio/iris_lm/keymaps/topfunky/keymap.c
+++ keyboards/keebio/iris_lm/keymaps/topfunky/keymap.c
@@ -1,52 +1,118 @@
 // Copyright 2023 Danny Nguyen (@nooges)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+// Personal modifications: 2025 Geoffrey Grosenbach (@topfunky)
+
 #include QMK_KEYBOARD_H
 
-enum custom_layers { _QWERTY, _LOWER, _RAISE, _MEDIA };
+enum custom_layers { _DVORAK, _LOWER, _RAISE, _MEDIA };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_QWERTY] = LAYOUT(
-        // ┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-        QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, DT_PRNT,
-        // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
-        // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_HOME, KC_END, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-        KC_LGUI, TL_LOWR, KC_ENT, KC_SPC, TL_UPPR, KC_RALT
-        // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
-        ),
+    [_DVORAK] = LAYOUT(
+        // Row 1
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,
+
+        KC_6, KC_7, KC_8, KC_9, KC_0, KC_LBRC,
+        // Row 2
+        KC_BSPC, KC_QUOT, KC_COMM, MT(MOD_LCTL, KC_DOT), MT(MOD_LALT, KC_P), KC_Y,
+
+        KC_F, MT(MOD_LALT, KC_G), MT(MOD_LCTL, KC_C), KC_R, KC_L, KC_RBRC,
+        // Row 3
+        KC_BSLS, KC_A, KC_O, MT(MOD_LGUI, KC_E), LT(1, KC_U), LT(2, KC_I),
+
+        LT(2, KC_D), LT(1, KC_H), MT(MOD_LGUI, KC_T), KC_N, KC_S, KC_MINS,
+        // Row 4, plus top thumb keys
+        KC_GRV, KC_SCLN, KC_Q, KC_J, KC_K, KC_X,
+
+        HYPR(KC_NO), MEH(KC_NO),
+
+        KC_B, KC_M, KC_W, KC_V, KC_Z, KC_SLSH,
+        // Row 5, remaining thumb keys
+        KC_LALT, MT(MOD_LSFT, KC_TAB), KC_ENT,
+
+        KC_SPC, KC_RGUI, MO(3)
+
+            ),
 
     [_LOWER] = LAYOUT(
-        // ┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-        KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
-        // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_GRV, _______, KC_UP, _______, QK_BOOT, _______, _______, KC_P7, KC_P8, KC_P9, KC_P0, KC_PGDN,
-        // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_DEL, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_LBRC, KC_RBRC, KC_P4, KC_P5, KC_P6, KC_PLUS, KC_PIPE,
-        // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-        RM_NEXT, EE_CLR, _______, _______, _______, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_P1, KC_P2, KC_P3, KC_MINS, _______,
-        // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-        _______, _______, KC_DEL, KC_DEL, _______, KC_P0
-        // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+        // Row 1
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
+
+        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+        // Row 2
+        _______, _______, _______, _______, _______, _______,
+
+        _______, _______, _______, _______, _______, _______,
+        // Row 3
+        _______, _______, _______, _______, _______, _______,
+
+        _______, _______, _______, _______, _______, _______,
+
+        // Row 4, plus top thumb keys
+        _______, _______, _______, _______, _______, _______,
+
+        KC_TRNS, KC_TRNS,
+
+        _______, _______, _______, _______, _______, _______,
+        // Row 5, remaining thumb keys
+        KC_TRNS, KC_TRNS, KC_TRNS,
+
+        KC_TRNS, KC_TRNS, KC_TRNS
+
         ),
 
     [_RAISE] = LAYOUT(
-        // ┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-        KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-        // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        RM_TOGG, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, QK_BOOT,
-        // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        RM_NEXT, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS, KC_EQL, KC_HOME, RM_HUEU, RM_SATU, RM_VALU, KC_BSLS,
-        // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, KC_LPRN, _______, KC_PLUS, KC_END, RM_HUED, RM_SATD, RM_VALD, EE_CLR,
-        // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-        _______, _______, _______, _______, _______, _______
-        // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+        // Row 1
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
+
+        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+        // Row 2
+        _______, _______, _______, _______, _______, _______,
+
+        _______, _______, _______, _______, _______, _______,
+        // Row 3
+        _______, _______, _______, _______, _______, _______,
+
+        _______, _______, _______, _______, _______, _______,
+
+        // Row 4, plus top thumb keys
+        _______, _______, _______, _______, _______, _______,
+
+        KC_TRNS, KC_TRNS,
+
+        _______, _______, _______, _______, _______, _______,
+        // Row 5, remaining thumb keys
+        KC_TRNS, KC_TRNS, KC_TRNS,
+
+        KC_TRNS, KC_TRNS, KC_TRNS
+
+        ),
+    [_MEDIA] = LAYOUT(
+        // Row 1
+        KC_TRNS, HYPR(KC_ENT), HYPR(KC_LEFT), HYPR(KC_RGHT), C(KC_UP), C(KC_DOWN),
+
+        _______, _______, _______, _______, _______, _______,
+        // Row 2
+        _______, _______, _______, _______, _______, _______,
+
+        _______, _______, _______, _______, _______, _______,
+        // Row 3
+        _______, KC_MPRV, KC_MNXT, KC_VOLU, _______, _______,
+
+        _______, _______, _______, _______, _______, _______,
+
+        // Row 4, plus top thumb keys
+        _______, KC_MSTP, KC_MPLY, KC_VOLD, _______, _______,
+
+        KC_TRNS, KC_TRNS,
+
+        _______, _______, _______, _______, _______, _______,
+        // Row 5, remaining thumb keys
+        KC_TRNS, KC_TRNS, KC_TRNS,
+
+        KC_TRNS, KC_TRNS, KC_TRNS
+
         )};
 
 void keyboard_post_init_user(void) {
@@ -63,61 +129,26 @@ void set_rgb_matrix(uint8_t hue, uint8_t sat, uint8_t val) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
         case _LOWER:
-            set_rgb_matrix(HSV_PURPLE);
+            // Must go to mode 1 to set solid color
+            rgb_matrix_mode(1);
+            set_rgb_matrix(HSV_CYAN);
             break;
         case _RAISE:
+            rgb_matrix_mode(1);
             set_rgb_matrix(HSV_GREEN);
             break;
         case _MEDIA:
-            set_rgb_matrix(HSV_ORANGE);
+            rgb_matrix_mode(1);
+            set_rgb_matrix(HSV_PURPLE);
             break;
-        default: //  for any other layers, or the default layer
-            set_rgb_matrix(HSV_CYAN);
+        default:
+            // Return to animation on default layer
+            rgb_matrix_mode(RGB_MATRIX_CYCLE_LEFT_RIGHT);
             break;
     }
 
     return state;
 }
-
-// // https://docs.qmk.fm/features/rgb_matrix
-// // HSV_CYAN HSV_ORANGE etc.
-// void keyboard_post_init_user(void) {
-//     rgblight_enable_noeeprom(); // enables Rgb, without saving settings
-//     rgblight_mode_noeeprom(1);
-//     layer_state_set_user(layer_state);
-// }
-
-// bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-//     for (uint8_t i = led_min; i < led_max; i++) {
-//         switch (get_highest_layer(layer_state | default_layer_state)) {
-//             case 2:
-//                 rgb_matrix_set_color(i, RGB_BLUE);
-//                 break;
-//             case 1:
-//                 rgb_matrix_set_color(i, RGB_YELLOW);
-//                 break;
-//             default:
-//                 break;
-//         }
-//     }
-//     return false;
-// }
-
-// // FIXME:Doesn't work
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//     switch (get_highest_layer(state)) {
-//         case _RAISE:
-//             rgb_matrix_sethsv(200, 255, 255);
-//             break;
-//         case _LOWER:
-//             rgb_matrix_sethsv(127, 255, 255);
-//             break;
-//         default: //  for any other layers, or the default layer
-//             rgb_matrix_sethsv(0, 255, 255);
-//             break;
-//     }
-//     return state;
-// }
 
 // // Enable/disable RGB matrix
 // rgb_matrix_enable();
@@ -135,5 +166,3 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 // // Change animation modes
 // rgb_matrix_mode(mode_number);
-// rgb_matrix_step();           // Next mode
-// rgb_matrix_step_reverse();   // Previous mode
